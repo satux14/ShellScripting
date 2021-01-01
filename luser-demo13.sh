1ccbaa84f38b2b0ee4f6de4ba2792605ee4d168e
vagrant <vagrant@localhost.localdomain>
2021-01-01 16:27:15 -0500

    grep/awk

---
+++ luser-demo13.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Display all open TCP ports
+# -4 for IPv4 TCP ports
+if [[ "${1}" = "-4" ]]
+then
+  netstat -nutl ${1} | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}'
+else
+  echo "Uknonwn option" >&2
+fi
+
